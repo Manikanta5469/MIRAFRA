e72f97964543b98a32c7e6d76f804b4c572c6cfc
Manikanta5469 <veeramanikanta@mirafra.com>
2024-05-18 15:32:14 +0530

    binary_search added

---
+++ c_prog/binary_search.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int binary_search(int arr[],int n,int s)
+{
+	int i=0,j=n-1,m;
+	while(i<=j)
+	{
+	m=(i+j)/2;
+	if(s>arr[m])
+		i=m+1;
+	else if(s<arr[m])
+		j=m-1;
+	else 
+		return m;		
+
+	}
+	return -1;
+
+}
+
+int main()
+
+{
+	int s,n,i,j;
+	printf("enter the size of array:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter array elements\n");
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&arr[i]);
+	}
+	int t=0;
+	
+	for(i=0;i<n;i++)
+	{
+	for(j=i+1;j<n;j++)
+	{
+		if(arr[i]>arr[j])	
+		{
+		t=arr[i];
+		arr[i]=arr[j];
+		arr[j]=t;
+		}
+	}
+	}
+	printf("after sorting\n");
+	for(i=0;i<n;i++)
+	{
+	printf("%d ",arr[i]);
+	}
+	printf("enter the search element:");
+	scanf("%d",&s);
+	int r=binary_search(arr,n,s);
+	printf("%d\n",r);
+	
+}
+
